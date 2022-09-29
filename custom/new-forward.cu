3d349f4f6a7dd814e1b63e0f2558b5294e818ba8
gguo4 <gguo4@illinois.edu>
2022-09-28 19:12:46 -0500

    add indentation

--- custom/new-forward.cu
+++ custom/new-forward.cu
@@ -31,16 +31,16 @@ __global__ void conv_forward_kernel(float *output, const float *input, const flo
     // float a = y4d(0,0,0,0)
     // y4d(0,0,0,0) = a
 
-#define out_4d(i3, i2, i1, i0) output[(i3) * (Map_out * Height_out * Width_out) + (i2) * (Height_out * Width_out) + (i1) * (Width_out) + i0]
-#define in_4d(i3, i2, i1, i0) input[(i3) * (Channel * Height * Width) + (i2) * (Height * Width) + (i1) * (Width) + i0]
-#define mask_4d(i3, i2, i1, i0) mask[(i3) * (Channel * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+    #define out_4d(i3, i2, i1, i0) output[(i3) * (Map_out * Height_out * Width_out) + (i2) * (Height_out * Width_out) + (i1) * (Width_out) + i0]
+    #define in_4d(i3, i2, i1, i0) input[(i3) * (Channel * Height * Width) + (i2) * (Height * Width) + (i1) * (Width) + i0]
+    #define mask_4d(i3, i2, i1, i0) mask[(i3) * (Channel * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
     // Insert your GPU convolution kernel code here
-   
+    
 
-#undef out_4d
-#undef in_4d
-#undef mask_4d
+    #undef out_4d
+    #undef in_4d
+    #undef mask_4d
 }
 
 	
