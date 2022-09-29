001bedd8b0241840f289d4a5af0912e64b4384fb
gguo4 <gguo4@illinois.edu>
2022-09-28 19:11:20 -0500

    add indentation

--- custom/cpu-new-forward.cc
+++ custom/cpu-new-forward.cc
@@ -2,26 +2,26 @@
 
 void conv_forward_cpu(float *output, const float *input, const float *mask, const int Batch, const int Map_out, const int Channel, const int Height, const int Width, const int K)
 {
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    The code in 16 is for a single image.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct, not fast (this is the CPU implementation.)
+  /*
+  Modify this function to implement the forward pass described in Chapter 16.
+  The code in 16 is for a single image.
+  We have added an additional dimension to the tensors to support an entire mini-batch
+  The goal here is to be correct, not fast (this is the CPU implementation.)
 
-    Function paramters:
-    output - output
-    input - input
-    mask - convolution kernel
-    Batch - batch_size (number of images in x)
-    Map_out - number of output feature maps
-    Channel - number of input feature maps
-    Height - input height dimension
-    Width - input width dimension
-    K - kernel height and width (K x K)
-    */
+  Function paramters:
+  output - output
+  input - input
+  mask - convolution kernel
+  Batch - batch_size (number of images in x)
+  Map_out - number of output feature maps
+  Channel - number of input feature maps
+  Height - input height dimension
+  Width - input width dimension
+  K - kernel height and width (K x K)
+  */
 
-    const int Height_out = Height - K + 1;
-    const int Width_out = Width - K + 1;
+  const int Height_out = Height - K + 1;
+  const int Width_out = Width - K + 1;
 
   // We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
   #define out_4d(i3, i2, i1, i0) output[(i3) * (Map_out * Height_out * Width_out) + (i2) * (Height_out * Width_out) + (i1) * (Width_out) + i0]
